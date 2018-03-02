ef2015b876763eb92b097e5c13d43c1c8f35f45e
jeffreywongo <wongojeffrey@yahoo.com>
2018-03-01 21:37:04 -0500

    added commas to attributes in shelter.js

--- models/shelter.js
+++ models/shelter.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Shelter = sequelize.define('Shelter', {
-    name: DataTypes.STRING
-    capacity: DataTypes.INTEGER
-    restrictions: DataTypes.STRING  // TODO: change to enum later
-    latitude: DataTypes.DECIMAL
-    longitude: DataTypes.DECIMAL
-    address: DataTypes.STRING       // Maybe split this up later
-    phone: DataTypes.STRING         // 1234567890 (first 3 area code, then rest)
+    name: DataTypes.STRING,
+    capacity: DataTypes.INTEGER,
+    restrictions: DataTypes.STRING,  // TODO: change to enum later
+    latitude: DataTypes.DECIMAL,
+    longitude: DataTypes.DECIMAL,
+    address: DataTypes.STRING,       // Maybe split this up later
+    phone: DataTypes.STRING,         // 1234567890 (first 3 area code, then rest)
     description: DataTypes.TEXT
   }, {});
   Shelter.associate = function(models) {
