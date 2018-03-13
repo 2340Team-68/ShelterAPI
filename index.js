5de72fd48b788cefd4a4df266188d29c3da29218
Adam Hayward <achayward1@gmail.com>
2018-03-13 11:47:41 -0400

    made db changes into a helper so that they can be different based on environment

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const models = require("./models");
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const routes = require("./routes/routes.js");
-const fs = require('fs');
-var parse = require('csv-parse');
+const EnvSetup = require("./helpers/env/envsetup")
 
 var app = express();
 
@@ -16,64 +14,7 @@ routes(app);
 
 
 var server = app.listen(3000, function () {
-    // sync database
-    models.sequelize.sync({
-        force: true,
-        logging: console.log
-    })
-        .then(() =>
-        // manually add check so capacity can't go below zero bcs sequelize and MySql are kinda shit at this
-        models.sequelize.query(
-            "CREATE TRIGGER trig_vacancies_check BEFORE UPDATE ON Shelters \n" +
-            "FOR EACH ROW \n" +
-            "BEGIN \n" +
-            "IF NEW.vacancies<0 THEN \n" +
-            "SET NEW.vacancies=0; \n" +
-            "CALL raise_error;\n" +
-            " END IF; \n" +
-            "END"
-        ))
-        // .then(() => models.HomelessPerson.create({ // this is a test
-        //     name: 'bob',
-        //     email: "bob@gmail.com",
-        //     password_hash: 'pass'
-        // }))
-        .then(function() {
-            // read the csv file
-            var inputFile = 'homeless_shelter_database.csv';
-            var parser = parse({delimiter: ','}, function (err, data) {
-                // when all shelter are available,then process them
-                // create an enum of restrictions
-                data.forEach(function(line) {
-                    if (line[0] === 'Unique Key') {
-                        return;
-                    }
-                    // get the string of restrictions
-                    var restrictions = line[3].split('/');
-                    var restrArray = [];
-                    var count = 0;
-                    restrictions.forEach(function(restr) {
-                        restrArray[count] = restr.toLowerCase();/* = restr.toLowerCase();*/
-                        count++;
-                    });
-                    var restrStr = restrArray.join(',');
-                    console.log(restrStr);
-                    // create database entry
-                    models.Shelter.create({
-                        "name": line[1],
-                        "capacity": parseInt(line[2]),
-                        "vacancies": parseInt(line[2]),
-                        "restrictions": restrStr,
-                        "longitude": parseFloat(line[4]).toPrecision(8),
-                        "latitude": parseFloat(line[5]).toPrecision(8),
-                        "address": line[6],
-                        "description": line[7],
-                        "phone": line[8]
-                    })
-                });
-            });
-            // read the inputFile, feed the contents to the parser
-            fs.createReadStream(inputFile).pipe(parser);
-        });
+    // sync database depending on enironment
+    EnvSetup.setup(app);
     console.log("app running on port.", server.address().port);
 });
